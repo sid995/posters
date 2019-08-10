e6686ca5c4f3bfde8ff19876bca1ecea133b4f5d
Siddharth Kundu <sk31395@gmail.com>
2019-08-10 23:35:36 +0530

    auth system for  app

---
+++ client/src/context/auth.js
@@ -0,0 +1,63 @@
+import React, { createContext, useReducer } from 'react'
+import jwtDecode from 'jwt-decode'
+
+const initialState = {
+  user: null
+}
+
+if (localStorage.getItem('jwtToken')) {
+  const decodedToken = jwtDecode(localStorage.getItem('jwtToken'))
+
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem('jwtToken')
+  } else {
+    initialState.user = decodedToken
+  }
+}
+
+const AuthContext = createContext({
+  user: null,
+  login: userData => {},
+  logout: () => {}
+})
+
+function authReducer(state, actions) {
+  switch (actions.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: actions.payload
+      }
+    case 'LOGOUT':
+      return {
+        ...state,
+        user: null
+      }
+
+    default:
+      return state
+  }
+}
+
+function AuthProvider(props) {
+  const [state, dispatch] = useReducer(authReducer, initialState)
+
+  function login(userData) {
+    localStorage.setItem('jwtToken', userData.token)
+    dispatch({
+      type: 'LOGIN',
+      payload: userData
+    })
+  }
+
+  function logout() {
+    localStorage.removeItem('jwtToken')
+    dispatch({
+      type: 'LOGOUT'
+    })
+  }
+
+  return <AuthContext.Provider value={{ user: state.user, login, logout }} {...props} />
+}
+
+export { AuthContext, AuthProvider }
