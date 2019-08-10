6733f2a9ae51fea06123147e9877c41cdc89eb5e
Siddharth Kundu <sk31395@gmail.com>
2019-08-10 23:34:24 +0530

    add AuthProvider

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,22 @@ import MenuBar from './components/MenuBar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import AuthRoute from './util/AuthRoute'
+
+import { AuthProvider } from './context/auth'
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <MenuBar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   )
 }
 
