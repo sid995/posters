fdebd90500100fec784dbdbef6dddf5ab54de888
Siddharth Kundu <sk31395@gmail.com>
2019-08-10 23:34:55 +0530

    Add menu system according auth

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/auth'
 
 function MenuBar() {
-  const pathname = window.location.pathname;
-  const path = pathname === '/' ? 'home' : pathname.substr(1);
+  const { user, logout } = useContext(AuthContext)
+  const pathname = window.location.pathname
+  const path = pathname === '/' ? 'home' : pathname.substr(1)
   const [activeItem, setActiveItem] = useState(path)
 
   const handleItemClick = (e, { name }) => setActiveItem(name)
 
-  return (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name={user.username} active as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
@@ -24,7 +33,7 @@ function MenuBar() {
           active={activeItem === 'login'}
           onClick={handleItemClick}
           as={Link}
-          to="/Login"
+          to="/login"
         />
         <Menu.Item
           name="register"
@@ -36,6 +45,8 @@ function MenuBar() {
       </Menu.Menu>
     </Menu>
   )
+
+  return menuBar
 }
 
 export default MenuBar
