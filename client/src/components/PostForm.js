841a10c60d4712e17cd55fcc10a675fb5a59e066
Siddharth Kundu <sk31395@gmail.com>
2019-09-01 21:44:58 +0530

    created form for new posts

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Form, Button } from 'semantic-ui-react'
+import gql from 'graphql-tag'
+import { useForm } from '../util/hooks'
+import { useMutation } from '@apollo/react-hooks'
+import { FETCH_POSTS_QUERY } from '../util/graphql'
+
+function PostForm() {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: ''
+  })
+
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY
+      })
+      data.getPosts = [result.data.createPost, ...data.getPosts]
+      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data })
+      values.body = ''
+    }
+  })
+
+  function createPostCallback() {
+    createPost()
+  }
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <h2>Create a post:</h2>
+      <Form.Field>
+        <Form.Input placeholder="Enter" name="body" onChange={onChange} value={values.body} />
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
+      </Form.Field>
+    </Form>
+  )
+}
+
+const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentCount
+    }
+  }
+`
+
+export default PostForm
